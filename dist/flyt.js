e9baf793c207f5bb24399c30d504c89e9ef9a680
Aftab Alam <aftabbuddy@gmail.com>
2017-02-21 17:10:04 +0530

    add 'dist' folder

---
+++ dist/flyt.js
@@ -0,0 +1,144 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define("flyt", [], factory);
+	else if(typeof exports === 'object')
+		exports["flyt"] = factory();
+	else
+		root["flyt"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/**
+ * Guts of `flatten` method 
+ * used to flatten an array of array(...of array, (...of, ...))
+ *
+ */
+var flatten = function flatten(sourceArr, targetArr) {
+    // initialize return array
+    if (!targetArr) {
+        targetArr = [];
+    }
+
+    for (var i = 0; i < sourceArr.length; i++) {
+        if (isArray(sourceArr[i])) {
+            flatten(sourceArr[i], targetArr);
+        } else {
+            targetArr.push(sourceArr[i]);
+        }
+    }
+
+    return targetArr;
+};
+
+module.exports = flatten;
+
+/**
+ * Private function to check if provided input `isArray`
+ * -- could be moved in separate file
+ */
+var isArray = function isArray(sourceArr) {
+    return Object.prototype.toString.call(sourceArr) === "[object Array]";
+};
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _flatten = __webpack_require__(0);
+
+var _flatten2 = _interopRequireDefault(_flatten);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Main library namespace to allow `extensions` in future
+ */
+var flyt = function flyt() {
+    return {
+        flatten: _flatten2.default
+    };
+};
+
+module.exports = flyt();
+
+/***/ })
+/******/ ]);
+});
